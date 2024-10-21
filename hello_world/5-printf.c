968300ea8ad04f24f4f01a2b7124251ed3c9fdfc
DMC254 <mercadodiego2006@gmail.com>
2024-10-21 16:14:01 -0400

    Program follow by line

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Description: Calls puts and recieves value returned by puts after its
- * execution.
- * Return: Always return 0 to indicate success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
