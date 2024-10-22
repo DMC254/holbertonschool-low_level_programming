0257ff801f94c2f99705f3866935cc453fd93d2a
DMC254 <mercadodiego2006@gmail.com>
2024-10-22 10:42:56 -0400

    various size 2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
