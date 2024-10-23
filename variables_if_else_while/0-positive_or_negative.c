28047a93d1b63d2103a5650754d223dae0a87393
DMC254 <mercadodiego2006@gmail.com>
2024-10-23 13:23:54 -0400

    random number 1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d \n", n);
-
-if (n > 0)
-{
-	printf("The number is positive \n");
-}
-else if (n < 0)
-{
-	printf("The number is negative \n");
-}
-else
-{
-	printf("The number is zero \n");
-}
-return (0);
+	
+	if (n > 0)
+	{
+		printf("The number is positive \n");
+	}
+	else if (n < 0)
+	{
+		printf("Thenumber is negative \n");
+	}
+	else
+	{
+		printf("The number is zero \n")
+	}
+	return (0);
 }
