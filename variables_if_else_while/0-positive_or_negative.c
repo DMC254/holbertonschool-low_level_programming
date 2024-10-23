0ce10b5b18723bb8bc65ae9887d22b765e9b1704
DMC254 <mercadodiego2006@gmail.com>
2024-10-23 18:36:01 -0400

    number random

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - assing random number
+ * main - assign random number
  *
- *Return 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
+
 	int n;
 
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
-	printf("%d \n", n);
+	printf("%d\n", n);
 
 	if (n > 0)
+
 	{
-		printf("The number is positive \n");
+
+		printf("The number is positive\n");
+
 	}
+
 	else if (n < 0)
+
 	{
-		printf("Then umber is negative \n");
+
+		printf("The number is negative\n");
+
 	}
+
 	else
+
 	{
-		printf("The number is zero \n");
+
+		printf("The number is zero\n");
+
 	}
+
 	return (0);
 }
