6a797ea9e118088aadb4a7a7bd3e08ff293d2a1a
DMC254 <mercadodiego2006@gmail.com>
2024-10-24 10:14:19 -0400

    number random greater that 5 etc

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,31 +11,24 @@ int main(void)
 {
 
 	int n;
-
 	int last_digit;
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
-	last_digit = n % 10;
-
-	if (last_digit < 0)
+	if ((n % 10) > 5)
 	{
 		last_digit = -last_digit;
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	printf("Last digit of %d is %d", n, last_digit);
-	if (last_digit > 5)
+	else if ((n % 10) > 5)
 	{
-		printf(" and is greater than 5\n");
-	}
-	else if (last_digit == 0)
-	{
-		printf(" and is 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	else
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
 }
