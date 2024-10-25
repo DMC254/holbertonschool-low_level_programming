3fbc133a679d63f4096c1cd0e8f887160bc078bd
DMC254 <mercadodiego2006@gmail.com>
2024-10-25 14:43:07 -0400

    prototype

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,5 +10,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int _putchar(char c);
 
 #endif
