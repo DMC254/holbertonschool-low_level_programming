96d8c230e1fe60f0528b4e0b130fa96f2cfee12c
DMC254 <mercadodiego2006@gmail.com>
2024-10-25 21:36:02 -0400

    value of an integer

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - computes the absolute value of an integer
  * @n: The integer to compute the absolute value of
- * 
+ *
  * Return: The absolute value of the integer
  */
 int _abs(int n)
