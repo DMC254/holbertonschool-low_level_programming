4709e237b0ca2fbb4c6d599555f21308ea8fddb6
DMC254 <mercadodiego2006@gmail.com>
2024-10-28 11:05:04 -0400

    print number 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print number form 0 to 9
+ *
+ * Return: 0 success
+ */
+void print_numbers(void)
+{
+	int number = 48;
+
+	for (number = 48; number < 57; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
+}
