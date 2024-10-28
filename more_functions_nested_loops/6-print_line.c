17ea168249ea2ef642a981cbdfeb2c6fee94a965
DMC254 <mercadodiego2006@gmail.com>
2024-10-28 14:01:32 -0400

    draw a line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
  * print_line - draw a straight line in the terminal
- * @n - number of times to print the character _.
+ * @n - The number of time to print the character '_'
  *
  * Return: void
  */
 void print_line(int n)
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	while (n-- > 0)
 	{
 		_putchar('_');
