b3ade080e3988b8b3c24615f162055a08a01630a
DMC254 <mercadodiego2006@gmail.com>
2024-10-30 10:07:53 -0400

    update value to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the variable pointer point 98
+ * @n: pointer of the vaiable
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
