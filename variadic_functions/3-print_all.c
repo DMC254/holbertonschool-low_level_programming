40f4fc7772a547bbb9cef05af1765841f0025130
DMC254 <mercadodiego2006@gmail.com>
2024-11-20 09:40:18 -0400

    fix 1

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,44 +10,42 @@
  */
 void print_all(const char * const format, ...)
 {
-	char *k;
-	va_list valist;
-	unsigned int i, j;
+va_list valist;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
 
-	i = 0;
-	while (format != NULL)
+	va_start(valist, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
 	{
-		va_start(valist, format);
-		while (format[i] != 0)
+		if (n  == (i - 1))
 		{
-			j = 1;
-			switch (format[i])
-			{
-				case 'c':
-				printf("%c", va_arg(valist, int));
-				break;
-				case 'i':
-				printf("%d", va_arg(valist, int));
-				break;
-				case 'f':
-				printf("%d", va_arg(valist, double));
-				break;
-				case 's':
-				k = va_arg(valist, char *);
-				if (k == 0)
-					k = "(mil)";
-				printf("%s", k);
-				break;
-				default:
-				j = 0;
-				break;
-			}
-			if (format[i + 1] && j)
-				printf(", ");
-			i++;
+			sep = "";
 		}
-		va_end(valist);
-		break;
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(valist, int), sep);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(valist, int), sep);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(valist, double), sep);
+			break;
+		case 's':
+			str = va_arg(valist, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", str, sep);
+			break;
+		}
+		n++;
 	}
 	printf("\n");
+	va_end(valist);
 }
