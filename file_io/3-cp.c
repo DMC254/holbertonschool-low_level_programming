1dbdc65b998cee159a12072632f2b02b7658ed16
DMC254 <mercadodiego2006@gmail.com>
2024-12-05 19:56:19 -0400

    copies content of file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,11 +16,9 @@ char *create_buffer(char *file)
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
-
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -33,11 +31,7 @@ char *create_buffer(char *file)
  */
 void close_file(int fd)
 {
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -68,31 +62,44 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
+
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		close_file(from);
+		exit(99);
+	}
 
 	do {
-		if (from == -1 || r == -1)
+		r = read(from, buffer, 1024);
+		if (r == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
+			close_file(from);
+			close_file(to);
 			exit(98);
 		}
 
 		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		if (w == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
+			close_file(from);
+			close_file(to);
 			exit(99);
 		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
 	} while (r > 0);
 
 	free(buffer);
@@ -101,3 +108,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
